704ec7e152a568f7252e606c4b9a193609a11ff7
Derek <112670195+dsmdex@users.noreply.github.com>
2025-03-20 03:00:06 -0700

    models registered to site

--- django_delight/inventory/admin.py
+++ django_delight/inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Ingredient)
+admin.site.register(models.MenuItem)
+admin.site.register(models.RecipeRequirements)
+admin.site.register(models.Purchases)
